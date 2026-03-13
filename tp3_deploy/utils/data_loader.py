94738afef142b525fe6567b01ead695aefb4fce8
Saad Achbar <saad_achbar@fr.mccormick.com>
2026-03-13 09:41:43 +0100

    TP3 - House Prices ML App 3

--- tp3_deploy/utils/data_loader.py
+++ tp3_deploy/utils/data_loader.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import streamlit as st
+from pathlib import Path
 
 
+
+
+# PAR
+ROOT = Path(__file__).parent.parent
 @st.cache_data
 def load_clean_data():
-    return pd.read_csv("data/house_prices_clean.csv")
-
-
+    return pd.read_csv(ROOT / "data" / "house_prices_clean.csv")
 @st.cache_data
 def load_raw_data():
-    return pd.read_csv("data/train.csv")
-
+    return pd.read_csv(ROOT / "data" / "train.csv")
 
 def to_csv_bytes(df: pd.DataFrame) -> bytes:
     return df.to_csv(index=False).encode("utf-8")
